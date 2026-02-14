a80cb01b7740614659d7d31383b6319c16eed983
Newburu <newburu@gmail.com>
2026-02-14 16:20:23 +0900

    Add config/master.key to linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,5 +11,5 @@ set :branch, "main"
 set :rbenv_type, :user
 set :rbenv_ruby, "4.0.0"
 
-append :linked_files, "config/database.yml", ".env"
+append :linked_files, "config/database.yml", ".env", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
